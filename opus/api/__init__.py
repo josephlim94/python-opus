b34c5e27a009d305e89736aebba4ba3545985209
josephlim_94 <josephlim_94@hotmail.co.uk>
2019-08-25 22:11:25 +0800

    fix libopus load on windows

--- opus/api/__init__.py
+++ opus/api/__init__.py
@@ -1,8 +1,14 @@
 import ctypes
 from ctypes.util import find_library
+from sys import platform
 
-
-libopus = ctypes.CDLL(find_library('opus'))
+if platform == "linux" or platform == "linux2":
+    libopus = ctypes.CDLL(find_library('opus'))
+elif platform == "win32":
+    if find_library('libopus-0'):
+        libopus = ctypes.CDLL(find_library('libopus-0'))
+    elif find_library('opus'):
+        libopus = ctypes.CDLL(find_library('opus'))
 
 c_int_pointer = ctypes.POINTER(ctypes.c_int)
 c_int16_pointer = ctypes.POINTER(ctypes.c_int16)
